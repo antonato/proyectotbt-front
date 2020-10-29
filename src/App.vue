f30d0099f38d0e8dc985dd59df85eb7f1a618753
Cesar <cesar.salazar@usach.cl>
2020-10-29 20:57:47 -0300

    Prueba conexion

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Ostia que me he conectado"/>
   </div>
 </template>
 
