97bc2d2e4636a5c2ecd2a9855bcdf482fc18a577
Anto <antonia.arriagada@usach.cl>
2020-11-20 19:40:09 -0300

    Se agrega axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueAxios from 'vue-axios';
+import Axios from 'axios';
 
 Vue.config.productionTip = false
 Vue.use(vuetify);
+Vue.use(VueAxios, Axios);
 
 new Vue({
   vuetify,
