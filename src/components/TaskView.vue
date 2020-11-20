fa211cc0f991aca2c1f9ed63bb504423e55226e9
César <cesar.salazar@usach.cl>
2020-11-20 20:10:54 -0300

    Conexión back-front: ver tarea

--- src/components/TaskView.vue
+++ src/components/TaskView.vue
@@ -1,13 +1,49 @@
 <template>
-  <v-data-iterator></v-data-iterator>
+  <v-container>
+
+    <v-layout justify-center>
+      <v-flex xs6>
+        <h2>Tarea</h2>
+          </v-flex>
+    </v-layout>
+
+    <v-layout justify-center>
+       <td><v-flex class="mr-5 mt-3">{{ task.nombre }}</v-flex></td>
+       <td><v-flex class="mr-5 mt-3">{{ task.finicio }}</v-flex></td>
+       <td><v-flex class="mr-5 mt-3">{{ task.ffin }}</v-flex></td>
+       <td><v-flex class="mr-5 mt-3">{{ task.descripcion }}</v-flex></td>
+    </v-layout>
+
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'TaskView',
   props: {
     msg: String
-  }
+  },
+
+  data: ()=>({
+    task:null
+  }),
+
+  mounted(){
+    this.getTask();
+  },
+
+  methods: {
+    getTask(){
+    axios.get('http://localhost:8081/tarea/getById/11')
+      .then( response => {
+        this.task = response.data
+      })
+      .catch( e=> console.log(e))
+    }
+  },
+
 }
 </script>
 
